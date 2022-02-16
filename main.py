43a0a46d3d84fc290bc84d5f77529c9add5437b0
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2022-02-16 22:02:46 +0000

    feat: many changes
    allow browser selection
    logging
    BREAKING CHANGE: change config format

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import argparse
+import logging
 import sys
 from os import path
 
 import toml
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 from module_scraper.download import Saver
 from module_scraper.vle import VLEWrapper
@@ -23,34 +23,65 @@ parser.add_argument("-H",
                     "--headless",
                     action="store_true",
                     help="Use headless mode")
+parser.add_argument(
+    "-b",
+    "--browser",
+    help="The browser driver to use, see the selenium docs",
+    choices=["firefox", "chrome", "ie", "edge", "safari"],
+    default="firefox")
+parser.add_argument("-l", "--log-level", choices=["debug", "info", "critical", "error", "warning"], default="warning", help="The logging level to use")
 
 if __name__ == "__main__":
     args = parser.parse_args()
+    logger = logging.getLogger("vle_getter")
+    logger.setLevel(args.log_level.upper())
 
     # Try to load config file
     try:
         app_options = toml.load(args.config_file)
     except FileNotFoundError:
-        error = f"""
+        logger.critical(f"""
 Specified config file doesn't exist.
-If you didn't give a config file argument, the script will look at '{DEFAULT_CONFIG}'."""
-        print(error)
+If you didn't give a config file argument, the script will look at '{DEFAULT_CONFIG}'.""")
         sys.exit(1)
 
     # Check data directory exists
     if not path.exists(app_options["paths"]["data"]):
-        print("The data path specified does not exist")
+        logger.critical("Data path '%s' does not exist", app_options["paths"]["data"])
         sys.exit(1)
 
-    # Setup selenium
-    sel_options = FirefoxOptions()
-    sel_options.headless = args.headless
-    vle = VLEWrapper(webdriver.Firefox(options=sel_options))
+    # Select a browser and setup selenium
+    driver = None
+    match args.browser:
+        case "firefox":
+            sel_options = webdriver.firefox.options.Options()
+            sel_options.headless = args.headless
+            driver = webdriver.Firefox(options=sel_options)
+        case "chrome":
+            sel_options = webdriver.chrome.options.Options()
+            sel_options.headless = args.headless
+            driver = webdriver.Chrome(options=sel_options)
+        case "edge":
+            sel_options = webdriver.edge.options.Options()
+            sel_options.headless = args.headless
+            driver = webdriver.Edge(options=sel_options)
+        case "ie":
+            sel_options = webdriver.ie.options.Options()
+            sel_options.headless = args.headless
+            driver = webdriver.Ie(options=sel_options)
+        case "safari":
+            sel_options = webdriver.safari.options.Options()
+            sel_options.headless = args.headless
+            driver = webdriver.Safari(options=sel_options)
+
+    # Setup the VLE infra
+    vle = VLEWrapper(driver)
     vle.login(**app_options["vle"]["login"])
     saver = Saver(app_options["paths"]["data"], app_options["paths"])
 
     # Get content
     for module in app_options["modules"]:
-        if module["announcements"]:
-            vle.goto_module_sidebar_link(module["name"], "Announcements")
-            saver.save(module["name"], "announcements", vle.driver.page_source)
+        for target in module["targets"]:
+            logging.info("Getting page {} from {}...", target["name"], module["name"])
+            vle.goto_module_sidebar_link(module["name"], target["name"])
+            saver.save(module["name"], target["name"], vle.driver.page_source)
